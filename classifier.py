d72754db9a551bebc1cc21bcc6566c30874705d6
Ary <aryclenio.barros@truckpad.com.br>
2021-04-03 16:46:51 -0300

    first commit

---
+++ classifier.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+import os
+
+orb = cv2.ORB_create(nfeatures=1000)
+
+path = 'images'
+images = []
+classNames = []
+imgList = os.listdir(path)
+print(imgList)
+for cl in imgList:
+    imgCur = cv2.imread(f'{path}/{cl}', 0)
+    images.append(imgCur)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+
+def findDes(images):
+    desList = []
+    for img in images:
+        kp, des = orb.detectAndCompute(img, None)
+        desList.append(des)
+    return desList
+
+
+def findId(img, desList, minMatch=25):
+    kp2, des2 = orb.detectAndCompute(img, None)
+    bf = cv2.BFMatcher()
+    matchList = []
+    finalVal = -1
+    try:
+        for des in desList:
+            matches = bf.knnMatch(des, des2, k=2)
+            good = []
+            for m, n in matches:
+                if m.distance < 0.75*n.distance:
+                    good.append([m])
+            matchList.append(len(good))
+    except:
+        pass
+    print(matchList)
+    if len(matchList) != 0:
+        if max(matchList) > minMatch:
+            finalVal = matchList.index(max(matchList))
+    return finalVal
+
+
+desList = findDes(images)
+print(len(desList))
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img2 = cap.read()
+    imgOriginal = img2.copy()
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    id = findId(img2, desList)
+    if id != -1:
+        cv2.putText(
+            imgOriginal, classNames[id], (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+    cv2.imshow('img2', imgOriginal)
+    cv2.waitKey(1)
+
+# img1 = cv2.imread('images/cat.jpg')
+# img2 = cv2.imread('images/cat2.jpg')
+# orb = cv2.ORB_create()
+
+# kp1, des1 = orb.detectAndCompute(img1, None)
+# kp2, des2 = orb.detectAndCompute(img2, None)
+
+# bf = cv2.BFMatcher()
+# matches = bf.knnMatch(des1, des2, k=2)
+# good = []
+# for m, n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+
+# img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
+
+
+# cv2.imshow('cat', img1)
+# cv2.imshow('cat2', img2)
+# cv2.imshow('match', img3)
+# cv2.waitKey(0)
